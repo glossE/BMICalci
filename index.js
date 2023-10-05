9a56ca0150238d16944a956a284cac8b4d7161ea
harsh <harshchavan172003@gmail.com>
2023-10-05 19:27:44 +0530

    JS added

---
+++ index.js
@@ -0,0 +1,10 @@
+var height= prompt("Enter your height:");
+var weight= prompt("Enter your weight:");
+
+var bmi = weight/height*height;
+
+//alert("Your BMI is "+bmi);
+
+var cal=document.getElementsByClassName("bmi");
+
+cal.textContent="hi";
